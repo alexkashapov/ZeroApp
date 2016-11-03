1c86965c44790bf23df312a8edbc610b632a9142
Alexander Kashapov <alex.kashapov@gmail.com>
2016-11-04 02:24:14 +0400

    add textview's gravity settings

--- app/src/main/java/com/zeroapp/fake/zeroapp/MainActivity.java
+++ app/src/main/java/com/zeroapp/fake/zeroapp/MainActivity.java
@@ -2,6 +2,7 @@ package com.zeroapp.fake.zeroapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.RelativeLayout;
@@ -25,6 +26,7 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 layout.setBackgroundColor(getResources().getColor(R.color.backgroundcolor1));;
                 label.setTextColor(getResources().getColor(R.color.labelcolor1));
+                label.setGravity(Gravity.CENTER);
             }
         });
 
@@ -33,6 +35,7 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 layout.setBackgroundColor(getResources().getColor(R.color.backgroundcolor2));;
                 label.setTextColor(getResources().getColor(R.color.labelcolor2));
+                label.setGravity(Gravity.RIGHT);
             }
         });
 
