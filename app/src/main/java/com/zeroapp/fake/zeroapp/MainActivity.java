e195edfe2f6c2a2063f70b8093223a8c62c2fbc5
Alexander Kashapov <alex.kashapov@gmail.com>
2016-11-07 00:33:26 +0400

    for land orientation

--- app/src/main/java/com/zeroapp/fake/zeroapp/MainActivity.java
+++ app/src/main/java/com/zeroapp/fake/zeroapp/MainActivity.java
@@ -1,5 +1,6 @@
 package com.zeroapp.fake.zeroapp;
 
+import android.content.res.Configuration;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Gravity;
@@ -9,6 +10,37 @@ import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    public int style=1;
+    public TextView label;
+    public RelativeLayout layout;
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        outState.putInt("style",style);
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        if(savedInstanceState.containsKey("style"))style=savedInstanceState.getInt("style");
+    }
+
+    @Override
+    protected void onResume() {
+
+        super.onResume();
+        switch(style) {
+            case 1:
+                layout.setBackgroundColor(getResources().getColor(R.color.backgroundcolor1));;
+                label.setTextColor(getResources().getColor(R.color.labelcolor1));
+                label.setGravity(Gravity.CENTER);
+                break;
+            case 2:
+                layout.setBackgroundColor(getResources().getColor(R.color.backgroundcolor2));;
+                label.setTextColor(getResources().getColor(R.color.labelcolor2));
+                label.setGravity(Gravity.RIGHT);
+                break;
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,9 +48,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final RelativeLayout layout = (RelativeLayout) findViewById(R.id.layout);
+        layout = (RelativeLayout) findViewById(R.id.layout);
+        label = (TextView) findViewById(R.id.label);
+
         Button fstyle = (Button) findViewById(R.id.fstylebt);
-        final TextView label = (TextView) findViewById(R.id.label);
         Button sstyle = (Button) findViewById(R.id.sstylebt);
 
         fstyle.setOnClickListener(new View.OnClickListener() {
@@ -27,6 +60,7 @@ public class MainActivity extends AppCompatActivity {
                 layout.setBackgroundColor(getResources().getColor(R.color.backgroundcolor1));;
                 label.setTextColor(getResources().getColor(R.color.labelcolor1));
                 label.setGravity(Gravity.CENTER);
+                style=1;
             }
         });
 
@@ -36,8 +70,8 @@ public class MainActivity extends AppCompatActivity {
                 layout.setBackgroundColor(getResources().getColor(R.color.backgroundcolor2));;
                 label.setTextColor(getResources().getColor(R.color.labelcolor2));
                 label.setGravity(Gravity.RIGHT);
+                style=2;
             }
         });
-
     }
 }
